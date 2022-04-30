8270ad882ed45be04f013e8691e7bf071a710c96
alexyyyyytytytytyt <95461634+alexyyyyytytytytyt@users.noreply.github.com>
2022-04-29 22:13:18 -0700

    added

--- static/app.js
+++ static/app.js
@@ -1,114 +1,139 @@
 // Initializes the page with a default plot
 function init() {
     d3.json("http://127.0.0.1:5000/get_data", function(response){
-        rank = []
-        rank2 = []
-        rank3 = []
-        rank4 = []
-        rank5 = []
-        rank6 = []
-        rank7 = []
-        rank8 = []
-        artist_name = []
-        artist_name2 = []
-        artist_name3 = []
-        artist_name4 = []
-        artist_name5 = []
-        artist_name6 = []
-        artist_name7 = []
-        artist_name8 = []
-        bar = []
+      function onlyUnique(value2, index2, self2) {
+        return self2.indexOf(value2) === index2;
+      }
+      let sampleYears = response.map(value2 => value2["Year"])
+      sampleYears = sampleYears.filter(onlyUnique);
+      console.log(sampleYears)
+
+      sampleYears.forEach((sample) => {
+      selector
+      .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
         console.log(response)
 
         d3.selectAll('div.panel-body').append('p').classed("mama", true).text('year: 2014')
         d3.selectAll('div.panel-body').append('p').classed("papa", true).text('x: rank')
         d3.selectAll('div.panel-body').append('p').classed("baby", true).text('y: artist names')
 
-        for (let j=2014; j<=2021; j++){
-            bar.push(j)
-        }
-        console.log(bar[7])
-
         function getyear(date) {
-            return date["Year"] == bar[0]
+            return date["Year"] == "2014"
         }
-        let data = response.filter(getyear);
-        
-        data.sort((a,b) => a["Rank"] - b["Rank"]); // b - a for reverse sort
-        console.log(data)
-        for (let i=0; i<data.length; i++){
-            console.log(data[i]["Rank"])
-            rank.push(data[i]["Rank"])
-            console.log(data[i]["Song Title"])
-            artist_name.push(data[i]["Song Title"])
-        }
+        let year_data = response.filter(getyear)
+        console.log(year_data)
+    
 
-        function getyear2(date) {
-            return date["Year"] == bar[1]
-        }
-        let data2 = response.filter(getyear2);
-        for (let j=0; j<data2.length; j++){
-            rank2.push(data2[j]["Rank"])
-            artist_name2.push(data2[j]["Song Title"])
-        }
-
-        function getyear3(date) {
-            return date["Year"] == bar[2]
-        }
-        let data3 = response.filter(getyear3);
-        for (let k=0; k<data3.length; k++){
-            rank3.push(data3[k]["Rank"])
-            artist_name3.push(data3[k]["Song Title"])
-        }
-
-        function getyear4(date) {
-            return date["Year"] == bar[3]
-        }
-        let data4 = response.filter(getyear4);
-        for (let k=0; k<data4.length; k++){
-            rank4.push(data4[k]["Rank"])
-            artist_name4.push(data4[k]["Song Title"])
-        }
-
-        function getyear5(date) {
-            return date["Year"] == bar[4]
-        }
-        let paimore = response.filter(getyear5);
-        for (let k=0; k<paimore.length; k++){
-            rank5.push(paimore[k]["Rank"])
-            artist_name5.push(paimore[k]["Song Title"])
-        }
-
-        function getyear6(date) {
-            return date["Year"] == bar[5]
-        }
-        let spymon = response.filter(getyear6);
-        for (let k=0; k<spymon.length; k++){
-            rank6.push(spymon[k]["Rank"])
-            artist_name6.push(spymon[k]["Song Title"])
-        }
-
-        function getyear7(date) {
-            return date["Year"] == bar[6]
-        }
-        let byemon = response.filter(getyear7);
-        for (let k=0; k<byemon.length; k++){
-            rank7.push(byemon[k]["Rank"])
-            artist_name7.push(byemon[k]["Song Title"])
-        }
-
-        function getyear8(date) {
-            return date["Year"] == bar[7]
-        }
-        let paimon = response.filter(getyear8);
-        for (let k=0; k<paimon.length; k++){
-            rank8.push(paimon[k]["Rank"])
-            artist_name8.push(paimon[k]["Artist Name"])
-        }
-        rank = rank.toString()
-        console.log(rank)
+        rank = year_data.map(my => my["Rank"])
+        artist_name = year_data.map(my => my["Song Title"])
         console.log(artist_name)
         
+    data = [{
+      x: rank,
+      y: artist_name,
+    type: 'bar'}];
+
+    var layout = {
+        title:'Rankings per Year'
+      };
+  
+    Plotly.newPlot("lineplot", data, layout);
+    });
+    let selector = d3.select("#selDataset");
+    d3.json("http://127.0.0.1:5000/singer_count", function(response){
+        let artist = response.map(row => row['Artist Name'])
+        let rank = response.map(row => row['Rank'])
+        console.log(rank)
+        var trace1 = {
+            x: artist,
+            y: rank,
+            type: 'scatter'
+          };
+          
+
+          var layout2 = {
+            title:'#s of Top 25 Artists 2014-2021'
+          };
+        var data = [trace1];
+        Plotly.newPlot('map', data, layout2);
+        
+    });
+    
+
+    d3.json("http://127.0.0.1:5000/pie", function(response){
+        function onlyUnique(value, index, self) {
+            return self.indexOf(value) === index;
+          }
+        let sampleNames = response.map(value => value["Artist Name"])
+        sampleNames = sampleNames.filter(onlyUnique);
+
+        console.log(sampleNames)
+    sampleNames.forEach((sample2) => {
+      incubator
+        .append("option")
+        .text(sample2)
+        .property("value", sample2);
+    });
+    
+        function find_singer(singer) {
+            // return player.madeTeam == true;
+            // A more concise way to express a boolean conditional
+            return singer["Artist Name"] == "Led Zeppelin";
+          }
+          
+          // Call the custom function with filter()
+          let singer_data = response.filter(find_singer);
+
+          years = singer_data.map(song => song["Year"])
+          songs = singer_data.map(song => song["Rank"])
+          console.log(songs)
+
+          var data = [{
+            values: songs,
+            labels: years,
+            type: 'pie'
+          }];
+
+          var layout = {
+            title:'Rank Percentage for Each Singer'
+          };
+
+          Plotly.newPlot('pie', data, layout);
+          
+    });
+    let incubator = d3.select("#pie_selector")
+      
+}
+  d3.selectAll("#selDataset").on("change", updatePlotly);
+  d3.selectAll("#pie_selector").on("change", updatePie);
+  // Call updatePlotly() when a change takes place to the DOM
+
+  
+  // This function is called when a dropdown menu item is selected
+function updatePlotly() {
+  d3.json("http://127.0.0.1:5000/get_data", function(response){
+    
+    // Use D3 to select the dropdown menu
+    var dropdownMenu2 = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu2.property("value");
+    // Initialize x and y arrays
+
+    function find_year(year) {
+      // return player.madeTeam == true;
+      // A more concise way to express a boolean conditional
+      return year["Year"] == dataset;
+    }
+    d3.select('div.panel-body>p').classed("mama", true).text('year: '+dataset)
+    let year_data = response.filter(find_year)
+    console.log(year_data)
+
+    rank = year_data.map(my => my["Rank"])
+    artist_name = year_data.map(my => my["Song Title"])
+
     data = [{
       x: rank,
       y: artist_name,
@@ -119,153 +144,15 @@ function init() {
       };
   
     Plotly.newPlot("lineplot", data, layout);
-    });
-    d3.json("http://127.0.0.1:5000/singer_count", function(response){
-        let artist = response.map(row => row['Artist Name'])
-        let rank = response.map(row => row['Rank'])
 
-        var trace1 = {
-            x: artist,
-            y: rank,
-            type: 'scatter'
-          };
-
-          var layout2 = {
-            title:'Artist Names vs. Rankings'
-          };
-        var data = [trace1];
-        Plotly.newPlot('map', data, layout2);
-        
-    });
-    let selector = d3.select("#pie_selector");
-
-    d3.json("http://127.0.0.1:5000/pie", function(response){
-        function onlyUnique(value, index, self) {
-            return self.indexOf(value) === index;
-          }
-        let sampleNames = response.map(value => value["Artist Name"])
-        sampleNames = sampleNames.filter(onlyUnique);
-
-        console.log(sampleNames)
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-        console.log(response)
-        function find_singer(singer) {
-            // return player.madeTeam == true;
-            // A more concise way to express a boolean conditional
-            return singer["Artist Name"] == "The Beatles";
-          }
-          
-          // Call the custom function with filter()
-          let singer_data = response.filter(find_singer);
-
-          years = singer_data.map(song => song["Year"])
-          songs = singer_data.map(song => song["Rank"])
-
-          var data = [{
-            values: songs,
-            labels: years,
-            type: 'pie'
-          }];
-
-          var layout = {
-            title:'Adding Names to Line and Scatter Plot'
-          };
-
-          Plotly.newPlot('pie', data, layout);
-    });
-}
-  
-  // Call updatePlotly() when a change takes place to the DOM
-  d3.selectAll("#selDataset").on("change", updatePlotly);
-  d3.selectAll("#pie_selector").on("change", updatePie);
-  
-  
-  // This function is called when a dropdown menu item is selected
-function updatePlotly() {
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-    // Initialize x and y arrays
-    var x = [];
-    var y = [];
-  
-    if (dataset === '2014') {
-      x = rank;
-      y = artist_name;
-      d3.selectAll('.mama').text('year: 2014')
-      d3.selectAll('.papa').text()
-      d3.selectAll('.baby').text()
-    }
-
-    else if (dataset === '2015') {
-      x = rank2;
-      y = artist_name2;
-      d3.selectAll('.mama').text('year: 2015')
-      d3.selectAll('.papa').text()
-      d3.selectAll('.baby').text()
-    }
-
-    else if (dataset === '2016') {
-        x = rank3;
-        y = artist_name3;
-        d3.selectAll('.mama').text('year: 2016')
-        d3.selectAll('.papa').text()
-        d3.selectAll('.baby').text()
-    }
-
-    else if (dataset === '2017') {
-        x = rank4;
-        y = artist_name4;
-        d3.selectAll('.mama').text('year: 2017')
-        d3.selectAll('.papa').text()
-        d3.selectAll('.baby').text()
-    }
-
-    else if (dataset === '2018') {
-        x = rank5;
-        y = artist_name5;
-        d3.selectAll('.mama').text('year: 2018')
-        d3.selectAll('.papa').text()
-        d3.selectAll('.baby').text()
-    }
-
-    else if (dataset === '2019') {
-        x = rank6;
-        y = artist_name6;
-        d3.selectAll('.mama').text('year: 2019')
-        d3.selectAll('.papa').text()
-        d3.selectAll('.baby').text()
-    }
-
-    else if (dataset === '2020') {
-        x = rank7;
-        y = artist_name7;
-        d3.selectAll('.mama').text('year: 2020')
-      d3.selectAll('.papa').text()
-      d3.selectAll('.baby').text()
-    }
-
-    else if (dataset === '2021') {
-        x = rank8;
-        y = artist_name8;
-        d3.selectAll('.mama').text('year: 2021')
-      d3.selectAll('.papa').text()
-      d3.selectAll('.baby').text()
-    }
   
     // Note the extra brackets around 'x' and 'y'
     Plotly.restyle("lineplot", "x", [x]);
     Plotly.restyle("lineplot", "y", [y]);
-
+  });
   }
 
-  function updatePie(){
+function updatePie(){
     d3.json("http://127.0.0.1:5000/pie", function(response){
         console.log(response)
         var dropdownMenu = d3.select("#pie_selector");
@@ -291,8 +178,7 @@ function updatePlotly() {
         });
   }
 
-  years = []
-  songs = []
+
 
   init();
 
